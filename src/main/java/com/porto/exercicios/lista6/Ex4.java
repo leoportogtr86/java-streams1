9fa545ad66d1488706f609a34e186ee9586bad4a
Leo Porto <leonardoreisporto@gmail.com>
2024-09-13 13:17:23 -0300

    Agrupamento de Dados com Collectors.groupingBy() em Java

---
+++ src/main/java/com/porto/exercicios/lista6/Ex4.java
@@ -0,0 +1,12 @@
+package com.porto.exercicios.lista6;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+//4. **Coletar em um Mapa:**
+//- Crie uma `List` de strings e use `collect()` para criar um `Map`,
+//onde a chave seja o comprimento da string e o
+//valor seja a própria string. Imprima o mapa resultante.
